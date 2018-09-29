5f4c43e6fa16c826604508e73267d2e34064cef0
Dylan George Statham <dylangeorgestatham@gmail.com>
2018-09-29 13:46:04 +1000

    Add flex and basic responsive layout

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -5,37 +5,53 @@ const Gallery = () => (
   <div className="Gallery">
     <div className="Gallery__row uk-child-width-expand@s uk-text-center">
       <div>
-        <div className="uk-card uk-card-default uk-card-body">
+        <div className="Gallery__item uk-card uk-card-default uk-card-body">
           What a beautiful sunrise
         </div>
       </div>
       <div>
-        <div className="uk-card uk-card-default uk-card-body">Item</div>
+        <div className="Gallery__item uk-card uk-card-default uk-card-body">
+          Item
+        </div>
       </div>
       <div>
-        <div className="uk-card uk-card-default uk-card-body">Item</div>
-      </div>
-    </div>
-    <div className="uk-child-width-expand@s uk-text-center" uk-grid>
-      <div>
-        <div className="uk-card uk-card-default uk-card-body">Item</div>
-      </div>
-      <div>
-        <div className="uk-card uk-card-default uk-card-body">Item</div>
-      </div>
-      <div>
-        <div className="uk-card uk-card-default uk-card-body">Item</div>
+        <div className="Gallery__item uk-card uk-card-default uk-card-body">
+          Item
+        </div>
       </div>
     </div>
-    <div className="uk-child-width-expand@s uk-text-center" uk-grid>
+    <div className="Gallery__row uk-child-width-expand@s uk-text-center">
       <div>
-        <div className="uk-card uk-card-default uk-card-body">Item</div>
+        <div className="Gallery__item uk-card uk-card-default uk-card-body">
+          Item
+        </div>
       </div>
       <div>
-        <div className="uk-card uk-card-default uk-card-body">Item</div>
+        <div className="Gallery__item uk-card uk-card-default uk-card-body">
+          Item
+        </div>
       </div>
       <div>
-        <div className="uk-card uk-card-default uk-card-body">Item</div>
+        <div className="Gallery__item uk-card uk-card-default uk-card-body">
+          Item
+        </div>
+      </div>
+    </div>
+    <div className="Gallery__row uk-child-width-expand@s uk-text-center">
+      <div>
+        <div className="Gallery__item uk-card uk-card-default uk-card-body">
+          Item
+        </div>
+      </div>
+      <div>
+        <div className="Gallery__item uk-card uk-card-default uk-card-body">
+          Item
+        </div>
+      </div>
+      <div>
+        <div className="Gallery__item uk-card uk-card-default uk-card-body">
+          Item
+        </div>
       </div>
     </div>
   </div>
