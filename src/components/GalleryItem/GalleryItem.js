ad8dfa0eaf7208122c5543b28d7c5d4d66e485be
Trevor Kilvington <trevorgk@gmail.com>
2018-10-01 20:18:43 +1000

    Replace stripped down css with complete uikit 3.0 version

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import './styles.css';
 
 const GalleryItem = ({ src, alt, text }) => {
   return (
-    <div className="Gallery__item uk-card uk-card-default uk-card-body">
-      <img src={src} alt={alt} />
-      <p>{text}</p>
-      <div class="uk-button-group">
-        <button class="uk-button uk-button-default">View</button>
-        <button class="uk-button uk-button-default">Edit</button>
+    <div className="GalleryItem uk-card uk-card-default">
+      <div className=" uk-card-body">
+        <img src={src} alt={alt} />
+        <p>{text}</p>
+        <div class="uk-button-group">
+          <button class="uk-button uk-button-default">View</button>
+          <button class="uk-button uk-button-default">Edit</button>
+        </div>
       </div>
     </div>
   );
